361f1dca7a728b60111ff6e9c4db272961f69a06
Uday Tiwari <udaytiwari360@outlook.com>
2023-04-19 19:50:56 +0530

    added structs-enuums/

---
+++ structs-enums/src/main.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+// Defining a 'User' Struct : 
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+
+    // Immutable 'User' instance or object 'u1'!
+    let u1 = User {
+        active : true,
+        username : String::from("Uday"),
+        email : String::from("uday@email.com"),
+        sign_in_count : 69,
+    };
+
+    // Shorthand syntax for structs!
+    let u2 = User {
+        active : false,
+        username : String::from("Uday"),
+        email : String::from("uday@email.com"),
+        ..u1 // Else same as 'u1'!
+    };
+
+    let white_col = Color(255, 255, 255);
+    let sun = AlwaysTrue;
+    
+    println!("u1 : {:?}", u1);
+    println!("u2 : {:?}", u2);
+    println!("white_col : {:?}", white_col);
+    println!("sun : {:?}", sun);
+}
+
+#[derive(Debug)]
+// Tuple Structs : 
+struct Color(u32, u32, u32);
+
+#[derive(Debug)]
+// Unit like structs : 
+struct AlwaysTrue;
