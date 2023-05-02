43fdd43cf103c304182cb603d189d4eabc04b42a
Uday Tiwari <udaytiwari360@outlook.com>
2023-05-02 19:09:29 +0530

    completed traits!

--- generic-traits-lifetimes/src/main.rs
+++ generic-traits-lifetimes/src/main.rs
@@ -1,7 +1,15 @@
 mod generic;
+mod traits;
+
 use generic::Point;
 use generic::max;
 
+use traits::NewsArticle;
+use traits::Tweet;
+use traits::Summary;
+use traits::notify;
+use traits::returns_summarizeable;
+use traits::Pair;
 
 fn main() {
 
@@ -17,5 +25,40 @@ fn main() {
 
     println!("{:?}", point_1.get_x());
     println!("{:?}", point_2.get_y());
+    
+    let tw = Tweet {
+        username : String::from("Uday Tiwari"),
+        content : String::from("Learning Rust!"),
+        is_reply : false,
+        is_retweet : false,
+    };
+
+    let tw1 = Tweet {
+        username : String::from("Uday Tiwari"),
+        content : String::from("Learning Rust!"),
+        is_reply : false,
+        is_retweet : false,
+    };
+
+    let news = NewsArticle {
+        author : String::from("Jakes"),
+        headline : String::from("Sky is falling"),
+        content : String::from("Sky is not actually falling"),
+    };
+
+    println!("Tweet Summary : {}", tw.summarize());
+    println!("NewsArticle Summary : {}", news.summarize());
+    println!("{}", tw.whois_auth());
+    println!("{}", news.whois_auth());
+
+    notify(&news);
+
+    println!("{}", returns_summarizeable().summarize());
+
+    let p1 = Pair::new(12.3, 33.6);
+    p1.cmp_display(); // Because f64 impl Display and PartialOrd traits!
+
+    let p2 = Pair::new(tw, tw1);
+    // p2.cmp_display(); // Error : Because 'Tweet' does not impl Display and PartialOrd traits!
     
 }  
---
+++ generic-traits-lifetimes/src/traits.rs
@@ -0,0 +1,106 @@
+// A trait defines functionality a particular type has and can share with other types. 
+// We can use traits to define shared behavior in an abstract way. 
+// We can use trait bounds to specify that a generic type can be any type that has certain behavior.
+
+//A type’s behavior consists of the methods we can call on that type. 
+// Different types share the same behavior if we can call the same methods on all of those types. 
+// Trait definitions are a way to group method signatures together to define a set of behaviors necessary to accomplish some purpose.
+
+use std::fmt::Display;
+
+pub struct NewsArticle {
+    
+    pub author : String,
+    pub headline : String,
+    pub content : String,
+
+}
+
+impl Summary for NewsArticle { 
+    // Uses Default Implementation for summarize() !
+
+    fn whois_auth(&self) -> String {
+        return format!("From {}.", self.author);
+    }
+}
+
+pub struct Tweet {
+
+    pub username : String,
+    pub content : String,
+    pub is_reply : bool,
+    pub is_retweet : bool,
+
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        return format!("{}, by {}", self.content, self.username);
+    }
+
+    fn whois_auth(&self) -> String {
+        return format!("Author of the tweet : {}", self.username);
+    }
+}
+
+pub trait Summary {
+
+    fn whois_auth(&self) -> String;
+
+    // Shared Methods Here :
+    fn summarize(&self) -> String {
+        // Default Implementation here :
+        return format!("Read more from {} ...", self.whois_auth());
+    }
+}
+
+// Traits as params :
+// pub fn notify(item : &impl Summary) -> () {
+//     println!("Breaking News : {}", item.summarize())
+// }
+
+//      -^----- Syntatic Sugar for this : Trait Bound Syntax
+pub fn notify<T: Summary>(item: &T) -> () {
+    println!("Breaking News : {}", item.summarize())
+}
+
+// Trait Bound with where clause :
+// pub fn some_function<T, U>(t: &T, u: &U) -> i32 where T: Display + Clone, U: Clone + Debug {
+//     todo!();
+// }
+
+// Returning types the on which the trait is implemented :
+//However, you can only use 'impl Trait' if you’re returning a single type.
+
+pub fn returns_summarizeable() -> impl Summary {
+    return Tweet {
+        username : String::from("@udayofc"),
+        content : String::from("Learning Rust!"),
+        is_reply : false,
+        is_retweet : false,
+    };
+} 
+
+// Using Trait Bounds to Conditionally Implement Methods ;
+// By using a trait bound with an impl block that uses generic type parameters, we can implement methods conditionally for types that implement the specified traits.
+
+pub struct Pair<T> {
+    x : T,
+    y : T,
+}
+
+impl<T> Pair<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    pub fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
